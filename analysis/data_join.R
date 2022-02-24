14c1fb7c41a1ee51d70a399402011c35d6fb7dfe
Linda Nab <linda.nab@thedatalab.org>
2022-02-24 16:17:30 +0000

    fix style code annotation

--- analysis/data_join.R
+++ analysis/data_join.R
@@ -1,28 +1,30 @@
-#############################################################
-## JOIN ETHNICITY DATA AND THE OTHER VARIABLES
-##
-## 
-## linda.nab@thedatalab.com - 2022024
-#############################################################
+## ###########################################################
 
-##---------------------Load libraries---------------------##
+##  This script:
+## - Imports the two data sets extracted from the cohort extractor
+## - Joins the two data sets
+
+## linda.nab@thedatalab.com - 2022024
+## ###########################################################
+
+# Load libraries ---
 library(here)
 library(dplyr)
 
-##-----------------------Load data------------------------##
+# Load data ---
 data <- 
   read.csv(here("output", "data", "input.csv"))
 data_ethnicity <- 
   read.csv(here("output", "data", "input_ethnicity.csv"))
 
-##----------------------Work horse------------------------##
-# Join data
+# Work horse ---
+## Join data
 data_joined <-
   data %>%
   left_join(data_ethnicity %>% select(patient_id, ethnicity),
             by = "patient_id") 
 
-##----------------------Save data-------------------------##
+# Save output ---
 saveRDS(data_joined, 
         here("output", "data", "data_joined.rds"),
         compress = TRUE)
