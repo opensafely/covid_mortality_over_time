bb7c16d81b01bb774d88317ecead4f140a42794c
Linda Nab <linda.nab@thedatalab.org>
2022-03-02 17:21:11 +0000

    feat: add some initial subgroup specific measures

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -618,4 +618,39 @@ measures = [
         denominator = "population",
         group_by = "population",
     ),
+    # calculate subgroup specific rates
+    Measure(
+        id = "age_mortality_rate",
+        numerator = "died_ons_covid_flag_any",
+        denominator = "population",
+        group_by = ["agegroup"],
+    ),
+
+    Measure(
+        id = "sex_mortality_rate",
+        numerator = "died_ons_covid_flag_any",
+        denominator = "population",
+        group_by = ["agegroup_std", "sex"],
+    ),
+
+    Measure(
+        id = "bmi_mortality_rate",
+        numerator = "died_ons_covid_flag_any",
+        denominator = "population",
+        group_by = ["agegroup_std", "sex", "bmi"],
+    ),
+
+    Measure(
+        id = "ethnicity_mortality_rate",
+        numerator = "died_ons_covid_flag_any",
+        denominator = "population",
+        group_by = ["agegroup_std", "sex", "ethnicity"],
+    ),
+
+    Measure(
+        id = "imd_mortality_rate",
+        numerator = "died_ons_covid_flag_any",
+        denominator = "population",
+        group_by = ["agegroup_std", "sex", "imd"],
+    ),
 ]
