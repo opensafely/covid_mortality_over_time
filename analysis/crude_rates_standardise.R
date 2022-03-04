f01203c754803267b9d020c21e2878f16a08bd33
Linda Nab <linda.nab@thedatalab.org>
2022-03-04 10:23:51 +0000

    refactor: use date in table io predefining dates

--- analysis/crude_rates_standardise.R
+++ analysis/crude_rates_standardise.R
@@ -10,28 +10,22 @@
 # Load libraries & functions ---
 library(here)
 library(readr)
-packageVersion('readr')
 library(lubridate)
 library(dplyr)
-### Load the function (create_seq_dates()) that returns a vector with a 
-### sequence of dates that is equal to the dates used in project.yaml to create 
-### the monthly cohorts
-source(here("analysis", "config.R"))
 
 # Import rates ---
 crude_rates <- 
   read_csv(file = here("output", "measure_crude_mortality_rate.csv"))
 
 # Standardise rates ---
-## STANDARDISE RATES TO 30 DAYS PM AND PER 100.000 INDIVIDUALS
-### Make a vector with the sequence of starting dates of the monthly cohorts:
-seq_dates <- create_seq_dates()
-### Make a vector containing the number of days in each cohort month:
-number_of_days_in_months <- days_in_month(seq_dates)
-### Standardise rates
+## Add column containing number of days in every month
 crude_rates <-
   crude_rates %>%
-  mutate(std_value = (value / number_of_days_in_months * 30) * 100000)
+  mutate(days_in_month = days_in_month(date))
+## Standardise monthly rates to 30 days per month and per 100.000 individuals
+crude_rates <-
+  crude_rates %>%
+  mutate(std_value = (value / days_in_month * 30) * 100000)
 
 # Save output ---
 output_dir <- here("output", "rates")
