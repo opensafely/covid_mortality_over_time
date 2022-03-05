339cc60bc91194685bea9ff24ad3c2826e431368
Linda Nab <linda.nab@thedatalab.org>
2022-03-05 15:46:18 +0000

    feat: remove dates and add vectors containing subgroups

--- analysis/config.R
+++ analysis/config.R
@@ -1,22 +1,41 @@
 ## ###########################################################
 
 ##  This script:
-## - creates a sequence of dates that is equal to the range of dates used by
-##   the argument --index-date-range "2020-02-01 to 2021-12-01 by month"
-##   in project.yaml
+## - Creates vectors of the demographics and comorbities
 
 ## linda.nab@thedatalab.com - 2022028
 ## ###########################################################
 
-### Config start date and end date
-create_seq_dates <- function(start_date = lubridate::ymd("20200201"), 
-                             end_date = lubridate::ymd("20200801")){
-  ### Calculate number of months between end date and start date:
-  number_of_months <- 
-    lubridate::interval(start_date, end_date) %/% months(1)
-  ### Create a sequence of dates, starting with start_date:
-  months <- seq(start_date, by = "month", length.out = number_of_months)
-  ### Add end_date to sequence:
-  months_including_end_date <- c(months, end_date)
-  return(months_including_end_date)
-}
+## Create vectors ---
+### Demographics
+demographics_vctr = c(
+  "bmi",
+  "ethnicity",
+  "smoking_status",
+  "imd",
+  "region")
+
+### Comorbidities
+comorbidities_vctr = c(
+  "hypertension",
+  "chronic_respiratory_disease",
+  "asthma",
+  "chronic_cardiac_disease",
+  "diabetes_controlled",
+  "cancer",
+  "haem_cancer",
+  "dialysis_kidney_transplant",
+  "ckd",
+  "chronic_liver_disease",
+  "stroke",
+  "dementia",
+  "other_neuro",
+  "organ_kidney_transplant",
+  "dysplenia",
+  "sickle_cell",
+  "ra_sle_psoriasis",
+  "aplastic_anaemia",
+  "permanent_immunodeficiency",
+  "temporary_immunodeficiency",
+  "learning_disability",
+  "sev_mental_ill")
