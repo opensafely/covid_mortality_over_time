f4595b7d42cc744c48a350e166bfb4cafd1132d7
Linda Nab <linda.nab@thedatalab.org>
2022-03-10 10:44:43 +0100

    style: delete white spaces

--- analysis/join_ethnicity.py
+++ analysis/join_ethnicity.py
@@ -10,6 +10,6 @@ for file in os.listdir("output"):
         if file.split("_")[1] not in ["ethnicity.csv.gz", "practice"]:
             file_path = os.path.join("output", file)
             df = pd.read_csv(file_path)
-            merged_df = df.merge(ethnicity_df, how = "left", on = "patient_id")
+            merged_df = df.merge(ethnicity_df, how="left", on="patient_id")
 
             merged_df.to_csv(file_path, index=False)
