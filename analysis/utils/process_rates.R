c0add16c9e4d9f645a9c6208ff16ab06bca0102e
Linda Nab <linda.nab@thedatalab.org>
2022-03-30 16:04:08 +0100

    feat: add levels of factors

--- analysis/utils/process_rates.R
+++ analysis/utils/process_rates.R
@@ -33,22 +33,94 @@ subgroups_rates_std <-
 names(subgroups_rates_std) <- subgroups_vctr
 
 # Change levels of factors (demographics + comorbidities) ---
+## BMI
 subgroups_rates_std[[which(names(subgroups_rates_std) == "bmi")]] <-
   subgroups_rates_std[[which(names(subgroups_rates_std) == "bmi")]] %>%
   mutate(bmi = fct_case_when(
-    bmi == "Not obese" ~ "Not"
+    bmi == "Not obese" ~ "Not obese",
+    bmi == "Obese I (30-34.9)" ~ "Obese I (30-34.9 kg/m2)",
+    bmi == "Obese II (35-39.9)" ~ "Obese II (35-39.9 kg/m2)",
+    bmi == "Obese III (40+)" ~ "Obese III (40+ kg/m2)",
+    TRUE ~ NA_character_
   ))
-
+## Ethnicity
+subgroups_rates_std[[which(names(subgroups_rates_std) == "ethnicity")]] <-
+  subgroups_rates_std[[which(names(subgroups_rates_std) == "ethnicity")]] %>%
+  mutate(ethnicity = fct_case_when(
+    ethnicity == 1 ~ "White - British",
+    ethnicity == 2 ~ "White - Irish",
+    ethnicity == 3 ~ "White - Any other White background",
+    ethnicity == 4 ~ "Mixed - White and Black Caribbean",
+    ethnicity == 5 ~ "Other",
+    ethnicity == 6 ~ "Unknown",
+    TRUE ~ NA_character_
+  ))
+## Smoking_status
+subgroups_rates_std[[which(names(subgroups_rates_std) == "smoking_status_comb")]] <-
+  subgroups_rates_std[[which(names(subgroups_rates_std) == "smoking_status_comb")]] %>%
+  mutate(smoking_status_comb = fct_case_when(
+    smoking_status_comb == "E" ~ "Former",
+    smoking_status_comb == "S" ~ "Current",
+    smoking_status_comb == "N + M" ~ "Never and unknown",
+    TRUE ~ NA_character_
+  ))
+# IMD
+subgroups_rates_std[[which(names(subgroups_rates_std) == "imd")]] <-
+  subgroups_rates_std[[which(names(subgroups_rates_std) == "imd")]] %>%
+  mutate(imd = fct_case_when(
+    imd == "1" ~ "1 (least)",
+    imd == "2" ~ "2",
+    imd == "3" ~ "3",
+    imd == "4" ~ "4",
+    imd == "5" ~ "5 (most)",
+    imd == "0" ~ NA_character_
+  ))
+# comorbidities
+subgroups_rates_std[[which(names(subgroups_rates_std) == "asthma")]] <-
+  subgroups_rates_std[[which(names(subgroups_rates_std) == "asthma")]] %>%
+  mutate(asthma = fct_case_when(
+    asthma == "0.0" ~ "No asthma",
+    asthma == "1.0" ~ "With no oral steroid use",
+    asthma == "2.0" ~ "With oral steroid use"
+  ))
+subgroups_rates_std[[which(names(subgroups_rates_std) == "diabetes_controlled")]] <-
+  subgroups_rates_std[[which(names(subgroups_rates_std) == "diabetes_controlled")]] %>%
+  mutate(diabetes_controlled = fct_case_when(
+    diabetes_controlled == "0.0" ~ "No diabetes",
+    diabetes_controlled == "1.0" ~ "Controlled",
+    diabetes_controlled == "2.0" ~ "Not controlled",
+    diabetes_controlled == "3.0" ~ "Without recent Hb1ac measure"
+  ))
+subgroups_rates_std[[which(names(subgroups_rates_std) == "dialysis_kidney_transplant")]] <-
+  subgroups_rates_std[[which(names(subgroups_rates_std) == "dialysis_kidney_transplant")]] %>%
+  mutate(dialysis_kidney_transplant = fct_case_when(
+    dialysis_kidney_transplant == "0.0" ~ "No dialysis",
+    dialysis_kidney_transplant == "1.0" ~ "With previous kidney transplant",
+    dialysis_kidney_transplant == "2.0" ~ "Without previous kidney transplant"
+  ))
+subgroups_rates_std[[which(names(subgroups_rates_std) == "ckd")]] <-
+  subgroups_rates_std[[which(names(subgroups_rates_std) == "ckd")]] %>%
+  mutate(ckd = fct_case_when(
+    ckd == "No CKD" ~ "No CKD",
+    ckd == "0" ~ "Stage 0",
+    ckd == "3a" ~ "Stage 3a",
+    ckd == "3b" ~ "Stage 3b",
+    ckd == "4" ~ "Stage 4",
+    ckd == "5" ~ "Stage 5"
+  ))
+subgroups_rates_std[[which(names(subgroups_rates_std) == "organ_kidney_transplant")]] <-
+  subgroups_rates_std[[which(names(subgroups_rates_std) == "organ_kidney_transplant")]] %>%
+  mutate(organ_kidney_transplant = fct_case_when(
+    organ_kidney_transplant == "No transplant" ~ "No transplant",
+    organ_kidney_transplant == "Kidney" ~ "Kidney transplant",
+    organ_kidney_transplant == "Organ" ~ "Other organ transplant"
+  ))
 # Create data.frame mapping reference levels of vars ---
 reference_values <- 
   cbind(subgroup = subgroups_vctr, reference = NA) %>%
   as.data.frame()
 
-reference_values <-
-  reference_values %>%
-  mutate(reference = case_when(
-    subgroup == "bmi" ~ "Not obese",
-    subgroup == "ethnicity" ~ ""
-    TRUE ~ ""
-  ))
-
+# Save output ---
+iwalk(.x = subgroups_rates_std,
+      .f = ~ write_csv(x = .x,
+                       path = here("output", "rates", paste0(.y, "_montlhy_std.csv"))))
