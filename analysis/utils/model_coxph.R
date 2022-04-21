9a46296c0af84c355dd6dd80678e6977768d53b9
Linda Nab <linda.nab@thedatalab.org>
2022-04-21 14:17:08 +0100

    feat: add error handling of coxph

--- analysis/utils/model_coxph.R
+++ analysis/utils/model_coxph.R
@@ -50,56 +50,66 @@ coxmodel <- function(data, variable) {
                                  "+ rcs(age, 4) + sex + strata(stp)"))
     n_vars <- 3
   }
-  log_file <- matrix(nrow = 1, ncol = 3) %>% as.data.frame()
-  colnames(log_file) <- c("variable", "warning_coxph", "error_cox.zph")
-  log_file[, 1] <- variable
-  # Cox regression
-  # returns function model() with components result, output, messages and warnings
-  model <- quietly(.f = ~ coxph(formula, data))
-  # Test PH assumption
-  # returns function test_ph() with components result and error
-  # if error, return matrix with NAs with the same
-  # dimensions as when there is no error
-  # nrow is 3 (variable, rcs(age,4) and sex) 
-  # plus 1 (global test) (= 4)
-  # ncol is 3, chisq, df and p
-  test_ph <- safely(.f = ~ cox.zph(model()$result)$table,
-                    otherwise = matrix(nrow = n_vars + 1, ncol = 3))
-  # output processing ---
-  # create vector with booleans (TRUE for main effect else FALSE) used to 
-  # select main effects from 'model'
-  selection <- model()$result$coefficients %>% names %>% startsWith(variable)
-  # count number of estimated main effects (levels of 'variable' minus one)
-  # which is used to create the data.frame 'out' with output
-  n_selection <- sum(selection)
   # init objects in which output is saved ---
   # create data.frame 'out' where output is saved 
-  # out has 3 columns with the HR and upper and lower limit of CI
+  # out has 5 columns with the HR and upper and lower limit of CI
   # and number of rows is equal to number of levels of 'variable' minus one
-  out <- matrix(nrow = n_selection, ncol = 5) %>% as.data.frame()
-  # give row and column names
+  if ({v <- data %>% pull(variable)} %>% is.factor){
+    n_levels_variable <- v %>% levels() %>% length()
+  } else if (v %>% is.logical()){
+    n_levels_variable <- 2
+  }
+  out <- matrix(nrow = n_levels_variable - 1, ncol = 5) %>% as.data.frame()
+  # give column names
   colnames(out) <- c("subgroup", "level", "HR", "LowerCI", "UpperCI")
+  # save variable for reference
+  out[, 1] <- rep(variable, n_levels_variable - 1)
   # create data.frame 'out_ph' where global test for ph assumption is saved
   out_ph <- matrix(nrow = 1, ncol = 2) %>% as.data.frame()
-  # give row and column names
+  # give column names
   colnames(out_ph) <- c("subgroup", "p")
-  # save output ---
-  # save coefficients of model and CIs in out
-  out[, 1] <- rep(variable, n_selection)
-  out[, 2] <- names(model()$result$coefficients)[selection] %>% 
-    sub(variable, "", .)
-  out[, 3] <- model()$result$coefficients[selection] %>% exp()
-  out[, 4:5] <- confint(model()$result)[selection,] %>% exp()
-  # save global test in 'out_ph'
+  # save variable for reference
   out_ph[1, 1] <- variable
-  out_ph[1, 2] <- test_ph()$result[n_vars + 1, 3]
-  # save warnings
-  if(length(model()$warnings) != 0){
-    log_file[, 2] <- model()$warnings
-  } else log_file[, 2] <- NA_character_
-  if(!is.null(test_ph()$error)){
-    log_file[, 3] <- test_ph()$error$message
-  } else log_file[, 3] <- NA_character_
+  # create data.frame 'log_file' where error and warning messages are saved
+  log_file <- matrix(nrow = 1, ncol = 3) %>% as.data.frame()
+  # give column names
+  colnames(log_file) <- c("subgroup", "warning_coxph", "error_cox.zph")
+  # save variable in 'log_file' for reference
+  log_file[1, 1] <- variable
+  # Cox regression
+  # returns function model() with components result, output, messages and warnings
+  model <- safely(.f = ~ quietly(.f = ~ coxph(formula, data)),
+                  otherwise = NULL)
+  if (!is.null(model()$result)){
+    # Test PH assumption
+    # returns function test_ph() with components result and error
+    # if error, return matrix with NAs with the same
+    # dimensions as when there is no error
+    # nrow is 3 (variable, rcs(age,4) and sex) 
+    # plus 1 (global test) (= 4)
+    # ncol is 3, chisq, df and p
+    test_ph <- safely(.f = ~ cox.zph(model()$result()$result)$table,
+                      otherwise = matrix(nrow = n_vars + 1, ncol = 3))
+    # output processing ---
+    # create vector with booleans (TRUE for main effect else FALSE) used to 
+    # select main effects from 'model'
+    selection <- model()$result()$result$coefficients %>% names %>% startsWith(variable)
+    # save output ---
+    # save coefficients of model and CIs in out
+    out[, 2] <- names(model()$result()$result$coefficients)[selection] %>% 
+      sub(variable, "", .)
+    out[, 3] <- model()$result()$result$coefficients[selection] %>% exp()
+    out[, 4:5] <- confint(model()$result()$result)[selection,] %>% exp()
+    # save global test in 'out_ph'
+    out_ph[1, 2] <- test_ph()$result[n_vars + 1, 3]
+    # save warnings
+    if(length(model()$result()$warnings) != 0){
+      log_file[, 2] <- model()$result()$warnings
+    } else log_file[, 2] <- NA_character_
+    if(!is.null(test_ph()$error)){
+      log_file[, 3] <- test_ph()$error$message
+    } else log_file[, 3] <- NA_character_
+  }
   print(out)
   print(out_ph)
   print(log_file)
@@ -132,7 +142,7 @@ coxmodel_list <- function(data, variables) {
     effect_estimates <-
       rbind(effect_estimates,
             coxmodels_output[[i]]$effect_estimates)
-    ph_test <- 
+    ph_tests <- 
       rbind(ph_tests,
             coxmodels_output[[i]]$ph_test)
     log_file <- 
@@ -140,6 +150,6 @@ coxmodel_list <- function(data, variables) {
             coxmodels_output[[i]]$log_file)
   }
   list(effect_estimates = effect_estimates, 
-       ph_tests = ph_test,
+       ph_tests = ph_tests,
        log_file = log_file)
 }
