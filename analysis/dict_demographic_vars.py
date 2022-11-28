537f50023033b391f5031f11a35bc69d87ee4142
Linda Nab <linda.nab@thedatalab.org>
2022-11-28 10:01:39 +0000

    add proxy for pop density

--- analysis/dict_demographic_vars.py
+++ analysis/dict_demographic_vars.py
@@ -272,4 +272,14 @@ demographic_variables = dict(
             },
         },
     ),
+    # Rurality
+    rural_urban=patients.address_as_of(
+      "index_data",
+      returning="rural_urban_classification",
+      return_expectations={
+        "rate": "universal",
+        "category": {"ratios": {1: 0.125, 2: 0.125, 3: 0.125, 4: 0.125, 5: 0.125, 6: 0.125, 7: 0.125, 8: 0.125}},
+        "incidence": 1,
+        },
+    ),
 )
