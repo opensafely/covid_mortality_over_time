6c15fb2a87887c68228211b32831b095665ce15c
Linda Nab <linda.nab@thedatalab.org>
2023-01-06 16:33:09 +0000

    change order of exclusion criteria

--- analysis/flowchart.R
+++ analysis/flowchart.R
@@ -29,38 +29,26 @@ data <- readRDS(rds_file)
 # Calc numbers
 total_n <- nrow(data) %>% plyr::round_any(5)
 
-# no follow-up of three months
-no_follow_up <- 
-  data %>% 
-  filter(has_follow_up == FALSE) %>% 
-  nrow() %>% plyr::round_any(5)
-# NA in has_follow_up?
-cat("#### any NA's in has_follow_up? ####\n")
-print(any(is.na(data$has_follow_up)))
-
-# follow-up but not 18 <= age <= 110
+# not 18 <= age <= 110
 no_age <- 
-  data %>% 
-  filter(has_follow_up == TRUE) %>%
+  data %>%
   filter(age < 18 | age > 110) %>% 
   nrow() %>% plyr::round_any(5)
 cat("\n#### any NA's in age? ####\n")
 print(any(is.na(data$age)))
 
-# follow-up & age but missing sex
+# age but missing sex
 no_sex <- 
   data %>%
-  filter(has_follow_up == TRUE) %>%
   filter(age >= 18 & age <= 110) %>% 
   filter(!(sex %in% c("F", "M"))) %>% 
   nrow() %>% plyr::round_any(5)
 cat("\n#### any NA's in sex? ####\n")
 print(any(is.na(data$sex)))
 
-# follow-up & age & sex but missing stp
+# age & sex but missing stp
 no_stp <- 
   data %>%
-  filter(has_follow_up == TRUE) %>%
   filter(age >= 18 & age <= 110) %>% 
   filter(sex %in% c("F", "M")) %>% 
   filter(is.na(stp)) %>% 
@@ -68,10 +56,9 @@ no_stp <-
 cat("\n#### any NA's in stp? ####\n")
 print(any(is.na(data$stp)))
 
-# follow-up & age & sex & stp but missing imd
+# age & sex & stp but missing imd
 no_imd <- 
   data %>%
-  filter(has_follow_up == TRUE) %>%
   filter(age >= 18 & age <= 110) %>% 
   filter(sex %in% c("F", "M")) %>% 
   filter(!is.na(stp)) %>% 
@@ -80,24 +67,37 @@ no_imd <-
 cat("\n#### any NA's in index_of_multiple_deprivation? ####\n")
 print(any(is.na(data$index_of_multiple_deprivation)))
 
+# age & sex & stp & imd but missing follow up
+no_follow_up <- 
+  data %>% 
+  filter(age >= 18 & age <= 110) %>% 
+  filter(sex %in% c("F", "M")) %>% 
+  filter(!is.na(stp)) %>% 
+  filter(index_of_multiple_deprivation != -1) %>%
+  filter(has_follow_up == FALSE) %>% 
+  nrow() %>% plyr::round_any(5)
+# NA in has_follow_up?
+cat("#### any NA's in has_follow_up? ####\n")
+print(any(is.na(data$has_follow_up)))
+
 # included
 total_n_included <- 
   data %>% 
-  filter(has_follow_up == TRUE) %>% 
   filter(age >= 18 & age <= 110) %>% 
   filter(sex %in% c("F", "M")) %>%
   filter(!is.na(stp)) %>%
-  filter(index_of_multiple_deprivation != -1) %>% 
+  filter(index_of_multiple_deprivation != -1) %>%
+  filter(has_follow_up == TRUE) %>%
   nrow() %>% plyr::round_any(5)
 
 # combine numbers
 out <-
   tibble(total_n,
-         no_follow_up,
          no_age,
          no_sex,
          no_stp,
          no_imd,
+         no_follow_up,
          total_n_included) 
 
 # Save output
